0bdf6b3f556050a2f4605b6a0e2c4777ad5ef00e
Patrick Martin <patrickmartin88@gmail.com>
2019-01-28 15:36:49 -0800

    Pause and Play work along with showing the location.

--- app/src/main/java/com/example/patrickjmartin/android_mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/patrickjmartin/android_mediaplayer/MainActivity.java
@@ -6,16 +6,14 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.VideoView;
@@ -25,7 +23,10 @@ public class MainActivity extends AppCompatActivity {
     MediaPlayer mediaPlayer;
     VideoView video;
     SeekBar seekBar;
-    ImageButton rewButton, playButton, stopButton, ffButton;
+    ImageButton rewButton, playButton, pauseButton, ffButton;
+    Handler handler;
+    double startTime, endTime;
+
 
 
     @Override
@@ -33,12 +34,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        handler = new Handler();
         mediaPlayer = new MediaPlayer();
         video = findViewById(R.id.video_view);
         seekBar = findViewById(R.id.seekBar);
         rewButton = findViewById(R.id.media_rew);
         playButton = findViewById(R.id.media_play);
-        stopButton = findViewById(R.id.media_pause);
+        pauseButton = findViewById(R.id.media_pause);
         ffButton = findViewById(R.id.media_ff);
 
         seekBar.setClickable(false);
@@ -58,30 +60,36 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mediaPlayer.start();
+                seekBar.setClickable(true);
 
+                startTime = mediaPlayer.getCurrentPosition();
+                endTime = mediaPlayer.getDuration();
+
+                seekBar.setMax((int) endTime);
+                seekBar.setProgress((int)startTime);
+                handler.postDelayed(updateSeekBar, 70);
 
             }
         });
 
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        pauseButton.setOnClickListener(new OnClickListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
+            public void onClick(View v) {
+                mediaPlayer.pause();
             }
         });
 
 
     }
+
+    private Runnable updateSeekBar = new Runnable() {
+        @Override
+        public void run() {
+            startTime = mediaPlayer.getCurrentPosition();
+            seekBar.setProgress((int)startTime);
+            handler.postDelayed(this, 70);
+        }
+    };
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
