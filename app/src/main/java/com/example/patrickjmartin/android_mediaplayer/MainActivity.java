30ce0c034c6f1f1b9f75b553ccd8180a0e4c1719
Patrick Martin <patrickmartin88@gmail.com>
2019-01-28 15:15:09 -0800

    Sound can now be chosen and played.

--- app/src/main/java/com/example/patrickjmartin/android_mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/patrickjmartin/android_mediaplayer/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.patrickjmartin.android_mediaplayer;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,25 +14,97 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+    VideoView video;
+    SeekBar seekBar;
+    ImageButton rewButton, playButton, stopButton, ffButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mediaPlayer = new MediaPlayer();
+        video = findViewById(R.id.video_view);
+        seekBar = findViewById(R.id.seekBar);
+        rewButton = findViewById(R.id.media_rew);
+        playButton = findViewById(R.id.media_play);
+        stopButton = findViewById(R.id.media_pause);
+        ffButton = findViewById(R.id.media_ff);
+
+        seekBar.setClickable(false);
+
+
+        FloatingActionButton musicFab = findViewById(R.id.add_media_playable);
+        musicFab.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("audio/* video/*");
+                startActivityForResult(intent, 1);
             }
         });
+
+        playButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+
+
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri uri = data.getData();
+        ContentResolver resolver = this.getContentResolver();
+        String type = resolver.getType(uri);
+
+        if(type.contains("audio")) {
+            mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+
+
+        } else {
+
+        }
+
+
+
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,5 +126,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mediaPlayer != null) {
+            mediaPlayer.release();
+        }
     }
 }
