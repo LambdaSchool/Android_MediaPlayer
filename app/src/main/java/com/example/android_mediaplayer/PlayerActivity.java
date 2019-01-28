967198add8b842f4eff0e0d02fd62c2739883b76
Jacob Seppanen <jbseppanen@gmail.com>
2019-01-28 13:52:53 -0800

    Player activity now plays audio when play button is pressed.

--- app/src/main/java/com/example/android_mediaplayer/PlayerActivity.java
+++ app/src/main/java/com/example/android_mediaplayer/PlayerActivity.java
@@ -1,16 +1,25 @@
 package com.example.android_mediaplayer;
 
 import android.database.Cursor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
+import java.io.IOException;
 
 public class PlayerActivity extends AppCompatActivity {
 
     public static final String SELECTED_MEDIA = "selection";
     Uri uri;
+    MediaPlayer mediaPlayer;
+    ImageButton playButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +33,44 @@ public class PlayerActivity extends AppCompatActivity {
         cursor.close();
 
         ((TextView) findViewById(R.id.player_text_name)).setText(displayText);
+
+        playButton = findViewById(R.id.player_button_play);
+        playButton.setEnabled(false);
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        try {
+            mediaPlayer.setDataSource(this, uri);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mediaPlayer.prepareAsync();
+
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                playButton.setEnabled(true);
+            }
+        });
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.stop();
+                } else {
+                    mediaPlayer.start();
+                }
+            }
+        });
+
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mediaPlayer.release();
+    }
+
 }
