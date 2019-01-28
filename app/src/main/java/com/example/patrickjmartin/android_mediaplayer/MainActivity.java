dfbfeaeaec2abd1968be1be5b2135d51a08290ce
Patrick Martin <patrickmartin88@gmail.com>
2019-01-28 15:43:32 -0800

    Seekbar will move to sepcific location.

--- app/src/main/java/com/example/patrickjmartin/android_mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/patrickjmartin/android_mediaplayer/MainActivity.java
@@ -56,6 +56,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mediaPlayer.seekTo(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
         playButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
