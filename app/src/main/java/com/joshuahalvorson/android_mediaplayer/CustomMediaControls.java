2017e3c94f24d2a8de995225dd44f82d012d2cc3
Josh Halvorson <Jhalvorson5@mail.greenriver.edu>
2019-01-28 12:45:57 -0800

    Create customMediaControls view and functionality to pick and play audio

---
+++ app/src/main/java/com/joshuahalvorson/android_mediaplayer/CustomMediaControls.java
@@ -0,0 +1,56 @@
+package com.joshuahalvorson.android_mediaplayer;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class CustomMediaControls extends LinearLayout{
+
+    private Button playPauseButton;
+
+    public CustomMediaControls(Context context) {
+        super(context);
+        init(null);
+    }
+
+    public CustomMediaControls(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public CustomMediaControls(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    public CustomMediaControls(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(attrs);
+    }
+
+    public void enablePlayPause(final MediaPlayer mediaPlayer){
+        playPauseButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                }else{
+                    mediaPlayer.start();
+                }
+            }
+        });
+
+
+    }
+
+    public void init(AttributeSet attrs){
+        setOrientation(HORIZONTAL);
+
+        playPauseButton = new Button(getContext());
+        addView(playPauseButton);
+
+    }
+}
--- app/src/main/java/com/joshuahalvorson/android_mediaplayer/MainActivity.java
+++ app/src/main/java/com/joshuahalvorson/android_mediaplayer/MainActivity.java
@@ -1,13 +1,52 @@
 package com.joshuahalvorson.android_mediaplayer;
 
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
+    private CustomMediaControls customMediaControls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        customMediaControls = findViewById(R.id.media_controls);
+
+
+        findViewById(R.id.add_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("audio/*");
+                startActivityForResult(intent, 1);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri uri = data.getData();
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        customMediaControls.enablePlayPause(mediaPlayer);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //mediaPlayer.release();
     }
 }
