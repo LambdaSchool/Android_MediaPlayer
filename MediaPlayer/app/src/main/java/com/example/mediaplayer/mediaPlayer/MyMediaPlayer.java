2269fe5036348b4cbd7733e3e608765a0bdec6fe
ThaDocsN <charles.tha.doc@gamil.com>
2019-01-29 04:03:36 -0600

    added method to initialize media player

--- MediaPlayer/app/src/main/java/com/example/mediaplayer/mediaPlayer/MyMediaPlayer.java
+++ MediaPlayer/app/src/main/java/com/example/mediaplayer/mediaPlayer/MyMediaPlayer.java
@@ -1,17 +1,56 @@
 package com.example.mediaplayer.mediaPlayer;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
 
+import java.io.IOException;
+
 public class MyMediaPlayer implements PlayerAdapter {
+    private int songId;
+
     public enum State{PLAYING, PAUSED, STOP, COMPLETED}
 
     private Context context;
     private MediaPlayer mediaPlayer;
+    private PlaybackInfoListener playbackInfoListener;
+
+    public void setPlaybackInfoListener(PlaybackInfoListener listener){
+        this.playbackInfoListener = listener;
+    }
+
+    private void initializePlayer(){
+        if (mediaPlayer == null){
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    if (playbackInfoListener != null){
+                        playbackInfoListener.onPlaybackCompleted();
+                    }
+                }
+            });
+        }
+    }
 
     @Override
     public void loadSong(int songId) {
+        this.songId = songId;
+        initializePlayer();
 
+        AssetFileDescriptor assetFile = context.getResources().openRawResourceFd(songId);
+
+        try {
+            mediaPlayer.setDataSource(assetFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            mediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
