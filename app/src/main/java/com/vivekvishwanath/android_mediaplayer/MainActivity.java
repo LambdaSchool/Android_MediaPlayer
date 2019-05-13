6d299621d74baea7b9f2d3419b5d6b14ad249f36
VivekV95 <vivekv210@gmail.com>
2019-05-13 15:55:05 -0500

    App now plays audio from selected file

--- app/src/main/java/com/vivekvishwanath/android_mediaplayer/MainActivity.java
+++ app/src/main/java/com/vivekvishwanath/android_mediaplayer/MainActivity.java
@@ -1,13 +1,43 @@
 package com.vivekvishwanath.android_mediaplayer;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button requestButton;
+    VideoView videoView;
+    private static final int AUDIO_REQUEST_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        videoView = findViewById(R.id.media_player);
+        requestButton = findViewById(R.id.request_button);
+        requestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("audio/*");
+                startActivityForResult(intent, AUDIO_REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == AUDIO_REQUEST_CODE && resultCode == RESULT_OK) {
+            videoView.setVideoURI(data.getData());
+            videoView.start();
+        }
     }
 }
